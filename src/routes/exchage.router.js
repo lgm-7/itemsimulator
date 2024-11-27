294dcbbc9f8c8ce7241f1662453577c8af284f7b
lgm-7 <xlzlrud@naver.com>
2024-11-27 19:59:27 +0900

    feaㅅ
    거래소라우터

---
+++ src/routes/exchage.router.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { prisma } from "../utils/prisma/index.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
